cd98602428c1c134313a1bf3332cb24c8d8a8ae1
Jay Phelps <hello@jayphelps.com>
2012-12-06 21:15:09 -0800

    removed profanity because that's just not nice now is it?

--- examples/javascript/backbone/js/app.js
+++ examples/javascript/backbone/js/app.js
@@ -2,22 +2,8 @@ $(function () {
     
     Hector.eval($("#templates").text());
 
-    /*Hector.Backbone.reopen.call(ExampleView, {
-
-        initialize: function() {
-            this.model.set({ name: "Example Name", iconClassName:"hello", label:"click me" });
-        }
-
-    });*/
-
-    var view = new ExampleView({ model: new Backbone.Model({label: "FUCK YPU"})});
+    var view = new ExampleView({ model: new Backbone.Model({ label: "Hello world" })});
 
     $("body").append(view.render().el);
-
-    var template = function (scope) {
-        alert(scope.fuck);
-    };
-
-    //template({ fuck: "you" });
 
 });
