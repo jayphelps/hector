7053f11ef916203298984df647e3e49a9be61e2d
Jay Phelps <hello@jayphelps.com>
2012-12-06 21:19:04 -0800

    added note about the future with pre-compiling

--- examples/javascript/backbone/js/app.js
+++ examples/javascript/backbone/js/app.js
@@ -1,5 +1,7 @@
 $(function () {
     
+    // Soon you'll be able to pre-compile and just include the output "binaries"
+    // as normal JavaScript, without needing to Hector.eval it.
     Hector.eval($("#templates").text());
 
     var view = new ExampleView({ model: new Backbone.Model({ label: "Hello world" })});
